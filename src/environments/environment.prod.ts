d45c6ca30017391bb8b57bb72192749d55e8b1b4
victor <g.victor.juan@gmail.com>
2019-08-05 15:51:13 -0300

    cambio de API_URL al servidor de 7ideas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     hmr       : false,
 
+    API            : 'http://server.sieteideas.com.ar:8080/ges-rrhh-svc/gesrh/',
     // API            : 'http://localhost:8083/gesrh/',
-    API            : 'http://localhost:8083/gesrh/',
     Cookie_User    : 'user', // Parametro que guarda el usuario logueado en la Cookie
     Cookie_Token   : 'token', // Parametro que guarda el token de autentificacion en la Cookie
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
     hmr       : false,
 
 
+    API            : 'http://server.sieteideas.com.ar:8080/ges-rrhh-svc/gesrh/',
     // API            : 'http://localhost:8083/gesrh/',
-    API            : 'http://localhost:8083/gesrh/',
     Cookie_User    : 'user', // Parametro que guarda el usuario logueado en la Cookie
     Cookie_Token   : 'token', // Parametro que guarda el token de autentificacion en la Cookie
 };
