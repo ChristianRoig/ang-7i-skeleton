59c4ace2af28f955600328545ee88166cf9da1bc
Juan <etcheverryjuancruz@gmail.com>
2019-08-06 10:01:20 -0300

    Agregar lista de gastos

---
+++ src/app/main/gastos/gasto-list/gasto-list.component.ts
@@ -0,0 +1,353 @@
+import { Component, OnDestroy, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatDialog, MatDialogRef, MatTableDataSource } from '@angular/material';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+import { fuseAnimations } from '@fuse/animations';
+import { FuseConfirmDialogComponent } from '@fuse/components/confirm-dialog/confirm-dialog.component';
+
+import { GastosService } from '../gastos.service';
+//import { GastoFormDialogComponent } from '../gastos-form/gastos-form.component';
+import { Gasto } from '../gasto.model';
+import { ContactsService } from 'app/main/contacts/contacts.service';
+
+export class Group {
+    level: number = 0;
+    parent: Group;
+    expanded: boolean = true;
+    get visible(): boolean {
+      return !this.parent || (this.parent.visible && this.parent.expanded);
+    }
+  }
+
+@Component({
+    selector     : 'gasto-list',
+    templateUrl  : './gasto-list.component.html',
+    styleUrls    : ['./gasto-list.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    animations   : fuseAnimations
+})
+export class GastoListComponent implements OnInit, OnDestroy
+{
+    @ViewChild('dialogContent')
+    dialogContent: TemplateRef<any>;
+
+    gastos: Gasto[] = [];
+    user: any;
+   // dataSource: FilesDataSource | null;
+   dataSource = new MatTableDataSource< Gasto | Group>([]);
+    displayedColumns = ['checkbox', 'avatar' ,'descripcion', 'proveedor', 'fecha', 'comprobante', 'estado', 'importe', 'buttons']; 
+    groupByColumn: string[] = ['periodo']
+
+   //displayedColumns = ['checkbox', 'avatar', 'name', 'email', 'phone', 'jobTitle'];
+    selectedContacts: any[];
+    checkboxes: {};
+    dialogRef: any;
+    confirmDialogRef: MatDialogRef<FuseConfirmDialogComponent>;
+
+    // Private
+    private _unsubscribeAll: Subject<any>;
+
+    /**
+     * Constructor
+     *
+     * @param {GastosService} _gastosService
+     * @param {MatDialog} _matDialog
+     */
+    constructor(
+        private _contactsService : ContactsService,
+        private _gastosService: GastosService,
+        public _matDialog: MatDialog,
+        private router: Router
+    )
+    {
+        // Set the private defaults
+        this._unsubscribeAll = new Subject();
+        
+/*         this.dataSource.data = this.addGroups(Gastos.gastos, this.groupByColumn);
+        this.dataSource.filterPredicate = this.customFilterPredicate.bind(this); */
+    }
+
+    customFilterPredicate(data: Gasto | Group, filter: string): boolean {
+        return (data instanceof Group) ? data.visible : this.getDataRowVisible(data);
+      }
+
+      getDataRowVisible(data: Gasto): boolean {
+        const groupRows = this.dataSource.data.filter(
+          row => {
+            if (!(row instanceof Group)) return false;
+            
+            let match = true;
+            this.groupByColumn.forEach(
+              column => {
+                if (!row[column] || !data[column] || row[column] !== data[column]) match = false;
+              }
+            );
+            return match;
+          }
+        );
+    
+        if (groupRows.length === 0) return true;
+        if (groupRows.length > 1) throw "Data row is in more than one group!";
+        const parent = <Group>groupRows[0];  // </Group> (Fix syntax coloring)
+    
+        return parent.visible && parent.expanded;
+      }
+
+      groupHeaderClick(row) {
+        row.expanded = !row.expanded
+        this.dataSource.filter = performance.now().toString();  // hack to trigger filter refresh
+      }
+    
+      addGroups(data: any[], groupByColumns: string[]): any[] {
+        var rootGroup = new Group();
+        return this.getSublevel(data, 0, groupByColumns, rootGroup);
+      }
+
+      getSublevel(data: any[], level: number, groupByColumns: string[], parent: Group): any[] {
+        // Recursive function, stop when there are no more levels. 
+        if (level >= groupByColumns.length)
+          return data;
+    
+        var groups = this.uniqueBy(
+          data.map(
+            row => {
+              var result = new Group();
+              result.level = level + 1;
+              result.parent = parent;
+              for (var i = 0; i <= level; i++)
+                result[groupByColumns[i]] = row[groupByColumns[i]];
+              return result;
+            }
+          ),
+          JSON.stringify);
+    
+        const currentColumn = groupByColumns[level];
+    
+        var subGroups = [];
+        groups.forEach(group => {
+          let rowsInGroup = data.filter(row => group[currentColumn] === row[currentColumn])
+          let subGroup = this.getSublevel(rowsInGroup, level + 1, groupByColumns, group);
+          subGroup.unshift(group);
+          subGroups = subGroups.concat(subGroup);
+        })
+        return subGroups;
+      }
+    
+      uniqueBy(a, key) {
+        var seen = {};
+        return a.filter(function (item) {
+          var k = key(item);
+          return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+        })
+      }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void
+    {
+        this._gastosService.onGastosChanged
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe(gastos => {
+                this.gastos = this.gastos.concat(gastos);
+                this.checkboxes = {};
+                 gastos.map(gasto => {
+                    this.checkboxes[gasto.id] = false;
+                }); 
+                this.dataSource.data = this.addGroups(this.gastos, this.groupByColumn);
+                this.dataSource.filterPredicate = this.customFilterPredicate.bind(this);
+            });
+    }
+
+    seeMore() : void {
+      this._gastosService.obtenerMasComprobantes();
+    }
+
+/*         this._contactsService.onSelectedContactsChanged
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe(selectedContacts => {
+                for ( const id in this.checkboxes )
+                {
+                    if ( !this.checkboxes.hasOwnProperty(id) )
+                    {
+                        continue;
+                    }
+
+                    this.checkboxes[id] = selectedContacts.includes(id);
+                }
+                this.selectedContacts = selectedContacts;
+            });
+
+        this._contactsService.onUserDataChanged
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe(user => {
+                this.user = user;
+            });
+
+        this._contactsService.onFilterChanged
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe(() => {
+                this._contactsService.deselectContacts();
+            }); */
+    
+
+    /**
+     * On destroy
+     */
+    ngOnDestroy(): void
+    {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Edit contact
+     *
+     * @param contact
+     */
+   /*  editContact(gasto: Gasto): void
+    {
+        let contacto = this._contactsService.getContactoByName(gasto.contacto_id);
+        this.dialogRef = this._matDialog.open(GastoFormDialogComponent, {
+            panelClass: 'gasto-form-dialog',
+            data      : {
+                action : 'edit',
+                gasto: gasto,
+                contact : contacto
+            }
+        });
+
+        this.dialogRef.afterClosed()
+            .subscribe(response => {
+                if ( !response )
+                {
+                    return;
+                }
+                const actionType: string = response[0];
+                const formData: FormGroup = response[1];
+                switch ( actionType )
+                {
+                    /**
+                     * Save
+                     * /
+                    case 'save':
+
+                      //  this._gastosService.updateContact(formData.getRawValue());
+
+                        break;
+                    /**
+                     * Delete
+                     * /
+                    case 'delete':
+
+                        this.deleteGasto(gasto);
+
+                        break;
+                }
+            });
+    } */
+
+    /**
+     * Delete Contact
+     */
+    deleteGasto(gasto): void
+    {
+        this.confirmDialogRef = this._matDialog.open(FuseConfirmDialogComponent, {
+            disableClose: false
+        });
+
+        this.confirmDialogRef.componentInstance.confirmMessage = 'Are you sure you want to delete?';
+
+        this.confirmDialogRef.afterClosed().subscribe(result => {
+            if ( result )
+            {
+                this._gastosService.deleteGasto(gasto);
+            }
+            this.confirmDialogRef = null;
+        });
+
+    }
+
+    verGasto(gasto: Gasto): void
+    {
+      this.router.navigate(['/gastos',gasto.id]);
+    }
+
+    /**
+     * On selected change
+     *
+     * @param contactId
+     */
+    onSelectedChange(contactId): void
+    {
+        this._gastosService.toggleSelectedContact(contactId);
+    }
+
+    /**
+     * Toggle star
+     *
+     * @param contactId
+     */
+    toggleStar(contactId): void
+    {
+        if ( this.user.starred.includes(contactId) )
+        {
+            this.user.starred.splice(this.user.starred.indexOf(contactId), 1);
+        }
+        else
+        {
+            this.user.starred.push(contactId);
+        }
+
+     //    this._gastosService.updateUserData(this.user);     
+    }
+
+    isGroup(index, item): boolean{
+        return item.level;
+      }
+
+}
+
+export class FilesDataSource extends DataSource<any>
+{
+    /**
+     * Constructor
+     *
+     * @param {ContactsService} _contactsService
+     */
+    constructor(
+        private _gastosService: GastosService
+    )
+    {
+        super();
+    }
+
+    /**
+     * Connect function called by the table to retrieve one stream containing the data to render.
+     * @returns {Observable<any[]>}
+     */
+    connect(): Observable<any[]>
+    {
+        return this._gastosService.onGastosChanged;
+    }
+
+    /**
+     * Disconnect
+     */
+    disconnect(): void
+    {
+    }
+}
---
+++ src/app/main/gastos/gastos.service.ts
@@ -0,0 +1,396 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+
+import { FuseUtils } from '@fuse/utils';
+
+import { Gasto } from './gasto.model';
+import { CookieService } from 'ngx-cookie-service';
+
+import { environment } from 'environments/environment';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+const API_URL : string = environment.API + 'compras';
+
+@Injectable()
+export class GastosService implements Resolve<any>
+{
+    onGastosChanged: BehaviorSubject<any>;
+    onSelectedGastosChanged: BehaviorSubject<any>;
+    onUserDataChanged: BehaviorSubject<any>;    
+    onSearchTextChanged: Subject<any>;
+    onFilterChanged: Subject<any>; 
+
+    gastos: Gasto[] = [];
+    infoOnChanged = new BehaviorSubject({});
+    info: any;
+    user: any;    
+    selectedGastos: string[] = [];
+
+    searchText: string;
+    filterBy: string;
+
+    index:number;
+    
+    httpOptions: any;
+    public static readonly MODULO: string = "Compras";
+    public static readonly CATEGORIA: string = "Facturas";
+    public static readonly ETIQUETA: string = "-Oficina-";
+    /**
+     * Constructor
+     *
+     */
+    constructor(
+        private http : HttpClient,
+        private cookieService: CookieService
+    )
+    {      
+        // Set the defaults
+        this.onGastosChanged = new BehaviorSubject([]);
+        this.onSelectedGastosChanged = new BehaviorSubject([]);
+/*      this.onUserDataChanged = new BehaviorSubject([]);*/        
+        this.onSearchTextChanged = new Subject();
+        this.onFilterChanged = new Subject();
+        this.index = 0;
+
+        this.httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                'Authorization': this.cookieService.get('tokenAuth')
+            })
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Resolver
+     *
+     * @param {ActivatedRouteSnapshot} route
+     * @param {RouterStateSnapshot} state
+     * @returns {Observable<any> | Promise<any> | any}
+     */
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any
+    {
+         return new Promise((resolve, reject) => {
+
+            Promise.all([
+                this.getGastos(),
+            ]).then(
+                ([files]) => {
+
+                     this.onSearchTextChanged.subscribe(searchText => {
+                        this.searchText = searchText;
+                        this.getGastos();
+                    });
+
+                    this.onFilterChanged.subscribe(filter => {
+                        this.filterBy = filter;
+                        this.getGastos();
+                    });
+ 
+                    resolve();
+
+                },
+                reject
+            );
+        }); 
+    }
+
+    getGastos(): Promise<any>
+    {
+         return new Promise((resolve, reject) => {
+                this.createRequestObtenerGastos()
+                    .subscribe((response: Gasto[]) => {
+                        this.gastos = response;
+                        if ( this.filterBy === 'frequent' ) //comentar este if
+                        {
+                            this.gastos = this.gastos.filter(_contact => {
+                                return this.user.frequentContacts.includes(_contact.id);
+                            });
+                        }  
+                        if ( this.searchText && this.searchText !== '' )
+                        {
+                            this.gastos = FuseUtils.filterArrayByString(this.gastos, this.searchText);
+                        }
+                         this.gastos = this.gastos.map(gasto => {                            
+                            return new Gasto(gasto);
+                        });   
+                        this.onGastosChanged.next(this.gastos);  
+                        resolve(this.gastos)
+
+                    }, reject);
+            }); 
+    }
+    
+    initGasto(gasto: Gasto): void {
+        gasto.modulo = GastosService.MODULO;
+        gasto.categoria = GastosService.CATEGORIA;
+        gasto.etiqueta = GastosService.ETIQUETA;
+        gasto.propietario = this.cookieService.get('userName');
+    }
+
+    addGasto(gasto): Promise<any> {
+        return new Promise((resolve, reject) => {
+
+            this.createRequestAddGasto(gasto)
+                .subscribe(response => {
+                    this.getGastos();
+                    resolve(response);
+                });
+        });
+    } 
+
+    deleteGasto(gasto): Promise<any> {
+        return new Promise((resolve, reject) => {
+
+            this.createRequestRemoveGasto(gasto)
+                .subscribe(response => {
+                    this.deleteContactList(gasto);
+                    this.getGastos(); 
+                    resolve(response);
+                });
+        });
+    }
+    
+    deleteContactList(gasto): void {
+        const contactIndex = this.gastos.indexOf(gasto);
+        this.gastos.splice(contactIndex, 1);
+        this.onGastosChanged.next(this.gastos);
+    }
+
+    getGastosByName(proveedor: string): Promise<any> {
+        let gastos: any[] = [];
+        return new Promise((resolve, reject) => {
+            this.createRequestGastosByProveedor(proveedor)
+                .subscribe((response: any) => {
+                    gastos = response;
+
+                    gastos = gastos.map(gasto => {
+                        return new Gasto(gasto);
+                    });
+                    resolve(gastos);
+                }, reject);
+        });
+    }
+
+    createRequestAddGasto(gasto: Gasto): any {
+
+        let url = API_URL + 'agregarGasto';
+        let request = JSON.stringify(gasto); //agrego un nuevo gasto. 
+
+        return this.http.post(url, request, this.httpOptions);
+    }
+
+    createRequestRemoveGasto(gasto: Gasto): any {
+
+        let url = API_URL + 'eliminarGasto';
+        let request = JSON.stringify(gasto); //remove contacto. 
+
+        return this.http.post(url, request, this.httpOptions);
+    }
+
+    createRequestObtenerGastos(): any {
+/*         let httpHeaders = new HttpHeaders({
+            'Content-Type': 'application/json',
+        });
+
+        let options = {
+            headers: httpHeaders
+        }; */
+        return this.http.get(API_URL); // post debido a que la cant de parametros para filtrar es mayor a 2.
+    }
+
+     createRequestGastosByProveedor( idProveedor: string): any {
+
+         let httpHeaders = new HttpHeaders({
+             'Content-Type': 'application/json',
+         });
+
+         let params = {
+             "proveedor": idProveedor
+         };
+
+
+         let options = {
+             headers: httpHeaders,
+             params : params
+         };
+
+         return this.http.get(API_URL, options);
+    } 
+    
+    getGasto(id: string) : Gasto {
+        let gasto: Gasto;
+        gasto = this.gastos.find(element =>  element.id == id  )
+        return gasto;    
+    }
+
+    obtenerMasComprobantes() {
+        this.index = this.index + 1;
+        this.getGastos();
+    }
+
+    getInfo(id:number): Promise<any> {
+ 
+
+    return null;
+    }
+
+    /**
+     * Get user data
+     *
+     * @returns {Promise<any>}
+     */
+/*      getUserData(): Promise<any>
+    {
+         return new Promise((resolve, reject) => {
+                this._httpClient.get('api/contacts-user/5725a6802d10e277a0f35724')
+                    .subscribe((response: any) => {
+                        this.user = response;
+                        this.onUserDataChanged.next(this.user);
+                        resolve(this.user);
+                    }, reject);
+            }
+        );
+        return null;
+    } */
+
+    /**
+     * Toggle selected contact by id
+     *
+     * @param id
+     */
+    toggleSelectedContact(id): void
+    {
+         // First, check if we already have that contact as selected...
+        if ( this.selectedGastos.length > 0 )
+        {
+            const index = this.selectedGastos.indexOf(id);
+
+            if ( index !== -1 )
+            {
+                this.selectedGastos.splice(index, 1);
+
+                // Trigger the next event
+                this.onSelectedGastosChanged.next(this.selectedGastos);
+
+                // Return
+                return;
+            }
+        } 
+
+        // If we don't have it, push as selected
+        this.selectedGastos.push(id);
+
+        // Trigger the next event
+        this.onSelectedGastosChanged.next(this.selectedGastos);
+    }
+
+    /**
+     * Toggle select all
+     */
+    toggleSelectAll(): void
+    {
+         if ( this.selectedGastos.length > 0 )
+        {
+            this.deselectGastos();
+        }
+        else
+        {
+            this.selectGastos();
+        } 
+    }
+
+    /**
+     * Select contacts
+     *
+     * @param filterParameter
+     * @param filterValue
+     */
+    selectGastos(filterParameter?, filterValue?): void
+    {
+        this.selectedGastos = [];
+
+        // If there is no filter, select all contacts
+        if ( filterParameter === undefined || filterValue === undefined )
+        {
+            this.selectedGastos = [];
+            this.gastos.map(contact => {
+                this.selectedGastos.push(contact.id);
+            });
+        }
+
+        // Trigger the next event
+        this.onSelectedGastosChanged.next(this.selectedGastos);
+    }
+
+    /**
+     * Update contact
+     *
+     * @param contact
+     * @returns {Promise<any>}
+     */
+/*     updateContact(contact): Promise<any>
+    {
+        return new Promise((resolve, reject) => {
+
+            this._httpClient.post('api/contacts-contacts/' + contact.id, {...contact})
+                .subscribe(response => {
+                    this.getGastos();
+                    resolve(response);
+                });
+        });
+    } */
+
+    /**
+     * Update user data
+     *
+     * @param userData
+     * @returns {Promise<any>}
+     */
+/*      updateUserData(userData): Promise<any>
+    {
+         return new Promise((resolve, reject) => {
+            this._httpClient.post('api/contacts-user/' + this.user.id, {...userData})
+                .subscribe(response => {
+                    this.getUserData();
+                    this.getGastos();
+                    resolve(response);
+                });
+        }); 
+        return null;
+    }  */
+
+    /**
+     * Deselect contacts
+     */
+    deselectGastos(): void
+    {
+         this.selectedGastos = [];
+
+        // Trigger the next event
+        this.onSelectedGastosChanged.next(this.selectedGastos); 
+    }
+
+    /**
+     * Delete selected contacts
+     */
+    deleteSelectedGastos(): void
+    {
+         for ( const contactId of this.selectedGastos )
+        {
+            const contact = this.gastos.find(_contact => {
+                return _contact.id === contactId;
+            });
+            const contactIndex = this.gastos.indexOf(contact);
+            this.gastos.splice(contactIndex, 1);
+        }
+        this.onGastosChanged.next(this.gastos);
+        this.deselectGastos(); 
+    }
+}
