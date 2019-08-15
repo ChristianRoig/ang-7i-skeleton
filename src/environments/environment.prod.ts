194cf3839e64fabf1d49148a804f31de57137363
victor <g.victor.juan@gmail.com>
2019-08-15 11:08:39 -0300

    Nuevas APIS de back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,10 @@ export const environment = {
     production: true,
     hmr       : false,
 
-    API            : 'http://server.sieteideas.com.ar:8080/ges-rrhh-svc/gesrh/',
-    // API            : 'http://localhost:8083/gesrh/',
+
+    API_LOG        : 'http://server.sieteideas.com.ar:8080/fava-auth-svc/fava-auth-svc/login',
+    API            : 'http://server.sieteideas.com.ar:8080/ges-rrhh-svc/ges-rrhh-svc/',
+
     Cookie_User    : 'user', // Parametro que guarda el usuario logueado en la Cookie
     Cookie_Token   : 'token', // Parametro que guarda el token de autentificacion en la Cookie
 };
