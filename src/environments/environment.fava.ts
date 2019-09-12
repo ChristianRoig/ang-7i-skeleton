4a92e91b44da8b1b17121b68dc2d31c173d544fe
victor <g.victor.juan@gmail.com>
2019-09-12 09:59:55 -0300

    armado de entorno fava para build

---
+++ src/environments/environment.fava.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    hmr       : false,
+
+    API_LOG        : 'http://server.sieteideas.com.ar:8080/fava-auth-svc/fava-auth-svc/login',
+    API            : 'http://server.sieteideas.com.ar:8080/ges-rrhh-svc/ges-rrhh-svc/',
+
+    Cookie_User    : 'user', // Parametro que guarda el usuario logueado en la Cookie
+    Cookie_Token   : 'token', // Parametro que guarda el token de autentificacion en la Cookie
+};
