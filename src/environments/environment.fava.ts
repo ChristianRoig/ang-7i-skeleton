90bf5c3026939c5c5ec6e0f34ef82c3f5130ecf8
victor <g.victor.juan@gmail.com>
2019-09-19 10:03:13 -0300

    agregadas url del entorno de fava

--- src/environments/environment.fava.ts
+++ src/environments/environment.fava.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     hmr       : false,
 
-    API_LOG        : 'http://server.sieteideas.com.ar:8080/fava-auth-svc/fava-auth-svc/login',
-    API            : 'http://server.sieteideas.com.ar:8080/ges-rrhh-svc/ges-rrhh-svc/',
+    API_LOG        : 'http://192.168.100.101:8080/fava-auth-svc/fava-auth-svc/login',
+    API            : 'http://192.168.100.101:8080/ges-rrhh-svc/',
 
     Cookie_User    : 'user', // Parametro que guarda el usuario logueado en la Cookie
     Cookie_Token   : 'token', // Parametro que guarda el token de autentificacion en la Cookie
