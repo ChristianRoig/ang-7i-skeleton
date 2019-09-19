ccde46067b79a4acddc26775a665d67d333a9a0b
victor <g.victor.juan@gmail.com>
2019-09-19 10:59:34 -0300

    agregada la url de fava

--- src/environments/environment.fava.ts
+++ src/environments/environment.fava.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     hmr       : false,
 
-    API_LOG        : 'http://192.168.100.101:8080/fava-auth-svc/fava-auth-svc/login',
+    API_LOG        : 'http://192.168.100.101:8080/fava-auth-svc/login',
     API            : 'http://192.168.100.101:8080/ges-rrhh-svc/',
 
     Cookie_User    : 'user', // Parametro que guarda el usuario logueado en la Cookie
