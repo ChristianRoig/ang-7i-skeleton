5bbffced04d8308d838b969ced3aebbc2678fab9
victor <g.victor.juan@gmail.com>
2019-09-27 14:27:04 -0300

    agregada imagen de empresa y modificado el modelo de contact

--- src/app/main/contacts/contact.model.ts
+++ src/app/main/contacts/contact.model.ts
@@ -20,6 +20,11 @@ export class Contact {
     notas: string;
     doc_nro: string;
     estado: string;
+    activo: string;
+    creado: string;
+    doc_tipo: string;
+    modificado: string;
+    predefinido: string;
 
     /**
      * Constructor
@@ -34,18 +39,45 @@ export class Contact {
             this.modulo = contact.modulo || '';
             this.categoria = contact.categoria || '';
             this.etiqueta = contact.etiqueta || '';
+            this.genero = contact.genero || '';
             this.nombre_corto = contact.nombre_corto || '';
-            this.file_link = contact.file_link || 'assets/images/avatars/profile.jpg';
+            this.file_link = this.defineAvatar(contact.file_link);
             this.nombre = contact.nombre || '';
             this.correo = contact.correo || '';
             this.telefono = contact.telefono || '';
             this.domicilio = contact.domicilio || '';
             this.localidad = contact.localidad || '';
             this.cond_iva = contact.cond_iva;
-            this.genero = contact.genero || '';
             this.notas = contact.notas || '';
             this.doc_nro = contact.doc_nro;
             this.estado = contact.estado || 'activo';
+            this.activo = contact.activo || null;
+            this.creado = contact.creado || '';
+            this.doc_tipo = contact.doc_tipo || '';
+            this.modificado = contact.modificado || '';
+            this.predefinido = contact.predefinido || null;
         }
+    }
+
+    private defineAvatar(imagen: string): string {        
+        if (imagen !== null && imagen !== '' && imagen !== undefined){
+            return imagen;
+        }
+        
+        if (this.genero.toLowerCase() === 'empresa'){
+            return 'assets/images/avatars/empresa.png';
+        }
+                
+        if (this.genero === 'mujer'){
+            return 'assets/images/avatars/avatarF.png';
+        }
+
+        if (this.genero === 'hombre') {
+            return 'assets/images/avatars/avatarF.png';
+        }
+
+        // Neutro o cualquier otra cosa
+        return 'assets/images/avatars/profile.jpg';
+
     }
 }
