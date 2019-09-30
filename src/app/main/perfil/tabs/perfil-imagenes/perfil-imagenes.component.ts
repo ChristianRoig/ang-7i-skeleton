7120a63151284d1de48af3e7d6e4ed86756897f9
victor <g.victor.juan@gmail.com>
2019-09-30 11:39:11 -0300

    modificado el tab de imagen del componente de perfil

--- src/app/main/perfil/tabs/perfil-imagenes/perfil-imagenes.component.ts
+++ src/app/main/perfil/tabs/perfil-imagenes/perfil-imagenes.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { PerfilService } from '../../perfil.service';
+import { takeUntil } from 'rxjs/operators';
+import { Contact } from 'app/main/contacts/contact.model';
 
 @Component({
   selector: 'perfil-imagenes',
   templateUrl: './perfil-imagenes.component.html',
   styleUrls: ['./perfil-imagenes.component.scss']
 })
-export class PerfilImagenesComponent implements OnInit {
+export class PerfilImagenesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  info: any;
 
-  ngOnInit() {
-  }
+// Private
+    private _unsubscribeAll: Subject<any>;
 
+    /**
+     * Constructor
+     *
+     * @param {PerfilService} _profileService
+     */
+    constructor(
+        private _profileService: PerfilService
+    )
+    {
+        // Set the private defaults
+        this._unsubscribeAll = new Subject();
+        this.info = new Contact({});
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * On init
+     */
+    ngOnInit(): void
+    {
+        this._profileService.infoOnChanged
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe(data => {
+              this.info = data;
+            });
+    }
+
+    /**
+     * On destroy
+     */
+    ngOnDestroy(): void
+    {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
 }
