2e87786dacc296e6ec18d632a4cbae675a224d6e
victor <g.victor.juan@gmail.com>
2019-10-07 12:23:33 -0300

    agregados nuevos campos al gasto.model para manejar la imagen del usuario asociado al gasto

--- src/app/main/comprobantes/gasto-view/gasto-view.component.ts
+++ src/app/main/comprobantes/gasto-view/gasto-view.component.ts
@@ -3,8 +3,6 @@ import { ActivatedRoute } from '@angular/router';
 import { fuseAnimations } from '@fuse/animations';
 import { ComprobantesService } from '../comprobantes.service';
 import { Gasto } from '../gasto.model';
-// import { Contact } from 'app/main/personas/contact.model';
-// import { PersonasService } from 'app/main/personas/personas.service';
 
 @Component({
   selector: 'gasto-view',
@@ -17,29 +15,17 @@ export class GastoViewComponent implements OnInit {
 
   gasto: Gasto;
   imagen: string;
-  
-  // private contacto: Contact;
 
-  constructor(
-        private _comprobantesService: ComprobantesService,
-        // private _contactService: PersonasService,
-        private activatedRoute: ActivatedRoute) {                  
-          this.imagen = 'assets/images/avatars/profile.jpg';    
+  constructor( private _comprobantesService: ComprobantesService,        
+               private activatedRoute: ActivatedRoute) {                  
+        this.imagen = 'assets/images/avatars/profile.jpg';    
    }
 
   ngOnInit(): void {
       this.activatedRoute.params.subscribe(params => {
         this.gasto = this._comprobantesService.getGasto(params['id']);
-  
-        // Esto esta re contra mal!! tendria que tener un join en el back que anide la imagen al gasto
-        // this.contacto = this._contactService.getContactos().find(element => element.id === this.gasto.contacto_id);
-  
-  
-  
-        // if (this.contacto) {
-        //   this.imagen = this.contacto.file_link;
-        // }
-  
+
+        this.imagen = this.gasto.contacto_avatar;
       });
   }
 
